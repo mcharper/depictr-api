2210560e09081dcb093bc2c572e7400e9ad4c858
Mike Harper <mike@mcharper.com>
2018-04-03 15:11:35 +0100

    Fix CORS header

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.set('view engine', 'jade');
 
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "https://mcharper.github.io");
-   res.header("Access-Control-Allow-Origin", "http://mcharper.github.io");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
