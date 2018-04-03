860b59df07ef538aebefa9efac3a2161d57bc2fb
Mike Harper <mike@mcharper.com>
2018-04-03 15:10:52 +0100

    Trigger another deploy

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
-
+ 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
