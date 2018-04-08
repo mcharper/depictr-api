de4653cb6739426cdd6c1f1227b24db3a8e0cde9
Mike Harper <mike@mcharper.com>
2018-04-08 13:19:55 +0100

    Set CORS header correctly for production

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Origin", "https://mcharper.github.io");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
+  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://mcharper.github.io");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
  
 // uncomment after placing your favicon in /public
