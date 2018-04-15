25d3795a1c5b9ffee606c12e124a3ea12f22b36b
Mike Harper <mike@mcharper.com>
2018-04-15 16:17:23 +0100

    Allow access from anywhere for now

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "https://mcharper.github.io");
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "https://mcharper.github.io");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
